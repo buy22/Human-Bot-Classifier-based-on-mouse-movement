e83c9ebe8941ad8db88639839ec78007c118d8e6
buy22 <90930892+buy22@users.noreply.github.com>
2021-12-10 21:06:20 -0600

    VAR model and new data

--- visualize.py
+++ visualize.py
@@ -3,7 +3,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def visual(path='Mousecollector/records.txt'):
+def visual1(path='data/dsjtzs_txfz_training.txt'):
+    train = pd.read_csv(path, sep=' ', header=None, encoding='utf-8', names=['id', 'data', 'target', 'label'])
+    data = train['data'].apply(lambda x: [list(map(float, point.split(',')))[:-1] for point in x.split(';')[:-1]])
+    human = list(data[:2600])
+    bot = list(data[2600:])
+    for line in bot:
+        x = [xy[0] for xy in line]
+        y = [xy[1] for xy in line]
+        plt.figure()
+        plt.plot(x, y)
+        plt.show()
+
+
+def visual2(path='Mousecollector/records.txt'):
     train = pd.read_csv(path, sep=' ', header=None, encoding='utf-8', names=['data'])
     data = train['data'].apply(lambda x: [list(map(float, point.split(','))) for point in x.split(';')[:-1]])
     for line in data:
@@ -14,5 +27,16 @@ def visual(path='Mousecollector/records.txt'):
         plt.show()
 
 
+def visual3(path='data/gc.csv'):
+    train = pd.read_csv(path, sep=' ', header=None, encoding='utf-8', names=['id','data','_','1','end'])
+    data = train['data'].apply(lambda x: [list(map(float, point.split(';'))) for idx, point in enumerate(x.split(',')) if idx % 2 == 0][2:-2])
+    for line in data:
+        x = [xy[0] for xy in line]
+        y = [xy[1] for xy in line]
+        plt.figure()
+        plt.plot(x, y)
+        plt.show()
+
+
 if __name__ == '__main__':
-    visual()
+    visual1()
